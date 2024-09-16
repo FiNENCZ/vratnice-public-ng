0b9f83f8608bbba84cb1398841ec05bc2a9daba8
o_kudela <kudela@diamo.cz>
2024-09-16 08:13:15 +0200

    refactoring

--- vratnice-public-ng/src/app/pages/povoleni-vjezdu-vozidla/povoleni-vjezdu-vozidla.component.ts
+++ vratnice-public-ng/src/app/pages/povoleni-vjezdu-vozidla/povoleni-vjezdu-vozidla.component.ts
@@ -72,7 +72,6 @@ export class PovoleniVjezduVozidlaComponent {
   detail?: PovoleniVjezduVozidlaDto = newPovoleniVjezduVozidlaDto();
   idZaznamu?: string;
   aktivita: boolean = true;
-  isRidicRequired: boolean = false;
   lokalitaList: LokalitaDto[] = [];
 
   showCsvInfoDialog = false;
