bf87edd2ad3e79766847ca0f3a3a8e6010ca5e62
o_kudela <kudela@diamo.cz>
2024-10-07 12:53:39 +0200

    oprava buggů

--- vratnice-public-ng/src/app/functions/get-error-message.function.ts
+++ vratnice-public-ng/src/app/functions/get-error-message.function.ts
@@ -2,6 +2,14 @@ export function getErrorMessage(error: any): string{
     const keys = Object.keys(error.error);
     if (error.error && error.error.message) {
       const message = error.error.message;
+
+     // Hledání klíče "message" a extrakce textu zprávy uvnitř
+      const messageMatch = message.match(/"message":"([^"]+)"/);
+      
+      if (messageMatch && messageMatch.length > 1) {
+        // Vrátíme nalezenou chybovou zprávu
+        return messageMatch[1];
+      }
   
       // Definujte regulární výraz pro extrakci chybové zprávy mezi \" a \"\""
       const regex = /"([^"]*)""/;
